caf22d3b58e8b34470969b52eab7949beb2cb564
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-27 23:54:34 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ require "pry"
 def nyc_pigeon_organizer(data)
   
   bird_names = data.values
-  bird_names.flatten.uniq
+  #bird_names.flatten.uniq
   bird_names.each do |names|
     binding.pry
   end
