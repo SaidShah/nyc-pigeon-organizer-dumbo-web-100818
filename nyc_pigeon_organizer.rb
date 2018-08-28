9f9f2857d253eec3041c0564354bb8b88755feaf
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-28 00:01:20 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,10 +3,7 @@ require "pry"
 def nyc_pigeon_organizer(data)
   
   all_names = []
-  data.each do |names, info|
-   all_names = info.values.flatten.uniq 
-  end
- 
+
  data.each {|name,info| all_names=info.values.flatten.uniq}
  
 end
